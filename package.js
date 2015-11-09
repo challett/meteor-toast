5f530b11403199a8e7c465fe233de7e91981b975
Connor Hallett <connor.hallett@gmail.com>
2015-11-09 10:40:32 -0500

    Updated package summary

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'challett:toast',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'This is a wrapper package for the toast css framework.  It adds a basic grid system to your project.',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/challett/meteor-toast.git',
   documentation: 'README.md'
